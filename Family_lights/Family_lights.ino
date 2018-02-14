154735d7f4dadce7e2eaf0c229b5119a3cc705ce
bizit524 <bizit524@gmail.com>
2018-02-14 23:47:11 +0000

    implement neopixels

--- Family_lights/Family_lights.ino
+++ Family_lights/Family_lights.ino
@@ -1,65 +1,86 @@
-#include <SPI.h>
-#include <Ethernet.h>
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Adafruit_NeoPixel.h>
 
-// Update these with values suitable for your network.
-byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(10,0,0,5);
-const char* server = "m23.cloudmqtt.com";
+#include <WiFiManager.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "D--------";
+const char* password =  "--------!";
+const char* mqttServer = "m------";
 const int mqttPort = 17393;
-const char* mqttUser = "--";
-const char* mqttPassword = "--";
+const char* mqttUser = "--------";
+const char* mqttPassword = "--------";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+#define PIXEL_PIN       4                      // Pin connected to the NeoPixel data input.
+#define PIXEL_COUNT     21                      // Number of NeoPixels.
+#define PIXEL_TYPE      NEO_GRB + NEO_KHZ800   // Type of the NeoPixels (see strandtest example).
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE); // create NeoPixels object
+
+
+
+void setup() {
+   // Initialize NeoPixels and turn them off.
+  pixels.begin();
+  
+  Serial.begin(115200);
+  
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi..");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+
+  
+  client.publish("Family_Lamps/Incoming_Colors", "Hello from ESP8266");
+  client.subscribe("Family_Lamps/Incoming_Colors");
+  
+
+
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
-Serial.print("Message arrived [");
-Serial.print(topic);
-Serial.print("] ");
-for (int i=0;i<length;i++) {
-Serial.print((char)payload[i]);
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
 }
-Serial.println();
-}
-
-EthernetClient ethClient;
-PubSubClient client(ethClient);
-
-void reconnect() {
-// Loop until we’re reconnected
-while (!client.connected()) {
-Serial.print("Attempting MQTT connection…");
-// Attempt to connect
-if (client.connect("")) {
-Serial.println("connected");
-// Once connected, publish an announcement…
-client.publish("outTopic","hello");
-// … and resubscribe
-client.subscribe("inTopic");
-} else {
-Serial.print("failed, rc=");
-Serial.print(client.state());
-Serial.println(" try again in 5 seconds");
-// Wait 5 seconds before retrying
-delay(5000);
-}
-}
-}
-
-void setup()
-{
-Serial.begin(57600);
-
-client.setServer(server, 19405);
-client.setCallback(callback);
-
-Ethernet.begin(mac, ip);
-// Allow the hardware to sort itself out
-delay(1500);
-}
-
-void loop()
-{
-if (!client.connected()) {
-reconnect();
-}
-client.loop();
+ 
+void loop() {
+  client.loop();
 }
