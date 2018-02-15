34b282e51280384d4431de6b1c8a81c90dc0c0a4
bizit524 <bizit524@gmail.com>
2018-02-15 14:08:58 +0000

    implemented button and debounce with publish

--- Family_lights/Family_lights.ino
+++ Family_lights/Family_lights.ino
@@ -2,11 +2,12 @@
 #include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
 
+//libraries for auto wifi setup
 #include <WiFiManager.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 
-
+//mqtt server variables 
 const char* mqttServer = "m23.cloudmqtt.com";
 const int mqttPort = 16743;
 const char* mqttUser = "crjhzcfb";
@@ -14,9 +15,19 @@ const char* mqttPassword = "OgX58L0IebEa";
  
 WiFiClient espClient;
 PubSubClient client(espClient);
+//cleint ID for hashing and debuging 
+const char* CELINT_ID = "1";
 
+  
+//define button pin and setup debounce
+const int BUTTON_PIN = 0;
+int buttonState = 0;
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 150;    // the debounce time; increase if the output flickers
 
-#define PIXEL_PIN       4                      // Pin connected to the NeoPixel data input.
+ 
+//setup of neopixels
+#define PIXEL_PIN       16                      // Pin connected to the NeoPixel data input.
 #define PIXEL_COUNT     21                      // Number of NeoPixels.
 #define PIXEL_TYPE      NEO_GRB + NEO_KHZ800   // Type of the NeoPixels (see strandtest example).
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE); // create NeoPixels object
@@ -24,16 +35,18 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE)
 
 
 void setup() {
-   // Initialize NeoPixels and turn them off.
+  // initialize the pushbutton pin as an input:
+  pinMode(BUTTON_PIN, INPUT);
+  // Initialize NeoPixels 
   pixels.begin();
   
   Serial.begin(115200);
   
   // We start by connecting to a WiFi network
    WiFiManager MyWifiManager;
-   MyWifiManager.autoConnect("WeatherFrame");
+   MyWifiManager.autoConnect("Family_Lamp");
   Serial.println("Connected to the WiFi network");
- 
+  //connects to mqtt server
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
  
@@ -53,10 +66,9 @@ void setup() {
     }
   }
 
-  
-  client.publish("Family_Lamps/Incoming_Colors", "Hello from ESP8266");
+  //publishes client id and subscribes to colors 
+  client.publish("Family_Lamps/Client_ID", CELINT_ID);
   client.subscribe("Family_Lamps/Incoming_Colors");
-  
 
 
 }
@@ -78,4 +90,16 @@ void callback(char* topic, byte* payload, unsigned int length) {
  
 void loop() {
   client.loop();
+  buttonState = digitalRead(BUTTON_PIN);
+    if ( (millis() - lastDebounceTime) > debounceDelay) 
+    {
+ 
+      //if the button has been pressed, lets toggle the LED from "off to on" or "on to off"
+      if (buttonState == LOW) 
+      {
+        client.publish("Family_Lamps/Incoming_Colors", "button press");
+        lastDebounceTime = millis(); //set the current time
+      }
+
+    }
 }
